07ceb8a33f748c9e765a7e26b6898117e1861121
Evan Hsu <evanhsu@gmail.com>
2022-09-10 17:57:10 -0600

    Support for multiple schools added. When multiple instances of this module are added to the MagicMirror config, they can keep their data separate now.

--- node_helper.js
+++ node_helper.js
@@ -7,24 +7,31 @@ module.exports = NodeHelper.create({
     var self = this;
     console.log("Starting node helper for: " + self.name);
 
-    self.config = {};
-    self.titanSchoolsClient = null;
+    // There will be a separate API client for each instance of this module (in case we want to poll
+    // multiple school menus)
+    self.titanSchoolsClients = {};
   },
 
-  fetchData: async function () {
+  fetchData: async function (instanceName) {
     var self = this;
 
-    if (!self.titanSchoolsClient) {
+    if (isObjectEmpty(self.titanSchoolsClients)) {
+      console.log(
+        `Skipping data fetch because there's no TitanSchools API clients configured`
+      );
       return;
     }
 
     try {
       //   const menu = await this.titanSchoolsClient.fetchMockMenu();
-      const menu = await this.titanSchoolsClient.fetchMenu();
-      this.sendSocketNotification("TITANSCHOOLS_FETCH_DATA_SUCCESS", menu);
+      const menu = await this.titanSchoolsClients[instanceName].fetchMenu();
+      this.sendSocketNotification(
+        `TITANSCHOOLS_FETCH_DATA_SUCCESS::${instanceName}`,
+        menu
+      );
     } catch (error) {
       this.sendSocketNotification(
-        "TITANSCHOOLS_FETCH_DATA_FAILED",
+        `TITANSCHOOLS_FETCH_DATA_FAILED::${instanceName}`,
         error.message
       );
       console.error(`Error response from the titanschools API: ${error}`);
@@ -37,24 +44,43 @@ module.exports = NodeHelper.create({
   socketNotificationReceived: function (notificationName, payload) {
     var self = this;
 
-    if (notificationName === "TITANSCHOOLS_SET_CONFIG") {
-      console.log(
-        `TitanSchools helper received notification: ${notificationName}`
-      );
-      self.config = payload;
-      self.titanSchoolsClient = new TitanSchoolsClient({
-        buildingId: self.config.buildingId,
-        districtId: self.config.districtId,
-        recipeCategoriesToInclude: self.config.recipeCategoriesToInclude
-      });
-      this.sendSocketNotification("TITANSCHOOLS_CLIENT_READY");
-    }
+    console.log(
+      `TitanSchools node_helper received notification: ${notificationName}`
+    );
 
-    if (notificationName === "TITANSCHOOLS_FETCH_DATA_REQUEST") {
-      console.log(
-        `TitanSchools helper received notification: ${notificationName}`
+    if (notificationName === "TITANSCHOOLS_SET_CONFIG") {
+      console.log(payload);
+      self.titanSchoolsClients[payload.instanceName] = new TitanSchoolsClient({
+        buildingId: payload.buildingId,
+        districtId: payload.districtId,
+        recipeCategoriesToInclude: payload.recipeCategoriesToInclude
+      });
+      this.sendSocketNotification(
+        `TITANSCHOOLS_CLIENT_READY::${payload.instanceName}`
       );
-      self.fetchData();
+    } else if (notificationName.includes(`TITANSCHOOLS_FETCH_DATA_REQUEST`)) {
+      self.fetchData(
+        extractInstanceNameSuffixFromNotification(notificationName)
+      );
     }
   }
 });
+
+/**
+ * Notifications are named with this pattern: ACTION_NAME::INSTANCE_NAME
+ * Where the ACTION_NAME will be something like "TITANSCHOOLS_FETCH_DATA_REQUEST"
+ * and the INSTANCE_NAME is the Titan Schools DistrictID concatenated with the BuildingID:
+ */
+function extractInstanceNameSuffixFromNotification(notificationName) {
+  return notificationName.split("::").pop();
+}
+
+function isObjectEmpty(obj) {
+  let propCount = 0;
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      propCount++;
+    }
+  }
+  return propCount === 0;
+}
