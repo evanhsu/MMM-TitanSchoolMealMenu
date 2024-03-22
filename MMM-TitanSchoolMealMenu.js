df7964918ecb02dff7c0a05a729740f6fee5e5ad
dathbe <github@beffa.us>
2024-03-22 10:09:07 -0700

    Update MMM-TitanSchoolMealMenu.js
    Add missing &&

--- MMM-TitanSchoolMealMenu.js
+++ MMM-TitanSchoolMealMenu.js
@@ -119,7 +119,7 @@ Module.register("MMM-TitanSchoolMealMenu", {
         } else if (dayMenu.label == "Sunday" && this.config.showWeekends == false) {
           this.config.numberOfDaysToDisplay = this.config.numberOfDaysToDisplay + 1;
           return;
-        } else if (dayMenu.label == "Today" && !dayMenu.lunch !dayMenu.breakfast && this.config.showWeekends == false) {
+        } else if (dayMenu.label == "Today" && !dayMenu.lunch && !dayMenu.breakfast && this.config.showWeekends == false) {
           this.config.numberOfDaysToDisplay = this.config.numberOfDaysToDisplay + 1;
           return;
         } else if (dayMenu.label == "Tomorrow" && !dayMenu.lunch && !dayMenu.breakfast && this.config.showWeekends == false) {
